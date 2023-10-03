7fdb17a5b95d01a446ad141cfca53a595997b29d
ricardomarkov <ricardomarkov@gmail.com>
2023-10-03 16:19:35 -0600

    Fixed data persistence using localstorage

--- src/App.js
+++ src/App.js
@@ -5,28 +5,47 @@ import { TodoList } from './TodoList';
 import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
-const defaultTodos=[
-  { text: '~ >Cortar cebolla', completed: true},
-  { text: '~ >Terminar el curso de React.js', completed: false},
-  { text: '~ >Hacer la cena', completed: true},
-  { text: '~ >Limpiar el cuarto', completed: false},
-  { text: '~ >Ordenar mi escritorio', completed: true}
-];
+// const defaultTodos=[
+//   { text: '~ >Cortar cebolla', completed: true},
+//   { text: '~ >Terminar el curso de React.js', completed: false},
+//   { text: '~ >Hacer la cena', completed: true},
+//   { text: '~ >Limpiar el cuarto', completed: false},
+//   { text: '~ >Ordenar mi escritorio', completed: true}
+// ];
+
+// localStorage.setItem('TODOPLANNER_V1',JSON.stringify(defaultTodos));
+// localStorage.removeItem('TODOPLANNER_V1');
 
 function App() {
-  const [todos, setTodos]=React.useState(defaultTodos)
 
+  const localStorageTodos = localStorage.getItem('TODOPLANNER_V1');
+  let parsedTodos;
+
+  if(!localStorageTodos){
+    localStorage.setItem('TODOPLANNER_V1', JSON.stringify([]));
+    parsedTodos=[];
+  }else{
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+
+  const [todos, setTodos]=React.useState(parsedTodos);
   const completedTodos = todos.filter(todo =>!!todo.completed).length;
   const totalTodos= todos.length;
-
   const [filterValue, setFilterValue] = React.useState('');
   
+  const storeTodo = (newTodos) => {
+    localStorage.setItem('TODOPLANNER_V1',JSON.stringify(newTodos));
+    
+    setTodos(newTodos);
+  }
+
   const completeTodo= (text)=>{
     const newTodos=[...todos];
     const todoIndex = newTodos.findIndex(
       (todo)=> todo.text === text);
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-    setTodos(newTodos);
+    storeTodo(newTodos);
   }
 
   const deleteTodo= (text)=>{
@@ -34,7 +53,7 @@ function App() {
     const todoIndex = newTodos.findIndex(
       (todo)=> todo.text === text);
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    storeTodo(newTodos);
   }
 
   const filteredTodos=todos.filter(
